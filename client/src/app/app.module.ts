85ad980609b71a4c5c6aba0a505b1a9ef1e1ac5a
TheLegendBreaker <thegreatcarradio@yahoo.com>
2019-10-27 00:15:51 -0600

    main game play menu is styled.
    next to get the game to display some models with the help of threejs. I need to work out how to scale the environment I found online. I also need to figure out how to apply the skin to my models. then it's on to animate.

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,9 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
 
 import { AppRoutingModule } from './app-routing.module';
 import * as root from './root';
 import * as game from './game';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -12,7 +14,9 @@ import * as game from './game';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    MatIconModule,
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [...root.components]
